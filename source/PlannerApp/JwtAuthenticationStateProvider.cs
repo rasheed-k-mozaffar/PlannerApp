9aa4d45f40cb833d7fec12f649fc98ecf69d13f6
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2022-11-14 19:35:47 +0300

    Implement authentication infrastructure

---
+++ source/PlannerApp/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+public class JwtAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly ILocalStorageService _storage;
+
+    public JwtAuthenticationStateProvider(ILocalStorageService storage)
+    {
+        _storage = storage;
+    }
+
+
+    public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        if (await _storage.ContainKeyAsync("access_token"))
+        {
+            var tokenAsString = await _storage.GetItemAsStringAsync("access_token");
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.ReadJwtToken(tokenAsString);
+            var identity = new ClaimsIdentity(token.Claims, "Bearer");
+            var user = new ClaimsPrincipal(identity);
+
+            var authState = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(authState));
+
+            return authState;
+        }
+
+        return new AuthenticationState(new ClaimsPrincipal());
+    }
+}
--- source/PlannerApp/Program.cs
+++ source/PlannerApp/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlannerApp;
 using Blazored.LocalStorage;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,12 +16,15 @@ builder.Services.AddHttpClient("PlanneApp.Api", client =>
     client.BaseAddress = new Uri("https//plannerapp-api.azurewebsites.net");
 }).AddHttpMessageHandler<AuthorizationMessageHandler>();
 builder.Services.AddTransient<AuthorizationMessageHandler>();
-
 builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
-
 
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+
 await builder.Build().RunAsync();
+
+
 
 
